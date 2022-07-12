454145ce283c3e73c84b45a2de9d0ca6134dcfdb
Dominic Rupert <dominicrupert@gmail.com>
2022-07-12 10:24:01 -0600

    fix

--- tower.client/vite.config.js
+++ tower.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../tower/tower/client',
+    outDir: '../tower/client',
     sourcemap: false
   },
   server: {
--- tower/client/app/env.js
+++
@@ -1,5 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'dev-3m-y0xxd.us.auth0.com'
-export const audience = 'https://DomDev.com'
-export const clientId = 'G4ZPiDNVdwquzBEPf1t4fIg2bBm6ucuM'
